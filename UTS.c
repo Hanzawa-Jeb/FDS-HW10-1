43416051625160926a6ee61987b38ed78a2c356d
Jeb_0816 <hz_legoman@hotmail.com>
2025-04-26 15:06:29 +0800

    Version_Corr

--- UTS.c
+++ UTS.c
@@ -2,12 +2,11 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#include <limits.h> 
+#include <limits.h>
 
 typedef struct AdjList {
-    int nextV;          
-    int edgeCap;        
-    int flow;           
+    int nextV;
+    int edgeCap; 
     struct AdjList *Next;
 } AdjList;
 typedef AdjList *AdjPtr;
@@ -24,22 +23,23 @@ int listTop = 0;
 int main() {
     char src[4], dest[4];
     int routeCnt;
-
     scanf("%3s %3s", src, dest);
     scanf("%d", &routeCnt);
 
-    char (*refList)[4] = (char (*)[4])calloc(2 * routeCnt, sizeof(char[4]));
-    AdjPtr *graph = (AdjPtr *)calloc(2 * routeCnt, sizeof(AdjPtr));
+    char (*refList)[4] = calloc(2 * routeCnt, sizeof(char[4]));
+    AdjPtr *graph = calloc(2 * routeCnt, sizeof(AdjPtr));
+
     createGraph(graph, routeCnt, refList);
 
     int srcIndex = getIndex(src, refList);
     int destIndex = getIndex(dest, refList);
+    int nodeCount = listTop;
 
-    int maxFlow = fordFulkerson(graph, 2 * routeCnt, srcIndex, destIndex);
+    int maxFlow = fordFulkerson(graph, nodeCount, srcIndex, destIndex);
     printf("%d", maxFlow);
 
     free(refList);
-    for (int i = 0; i < 2 * routeCnt; i++) {
+    for (int i = 0; i < nodeCount; i++) {
         AdjPtr temp = graph[i];
         while (temp) {
             AdjPtr toFree = temp;
@@ -48,7 +48,6 @@ int main() {
         }
     }
     free(graph);
-
     return 0;
 }
 
@@ -59,69 +58,53 @@ int getIndex(char name[4], char (*refList)[4]) {
         }
     }
     strcpy(refList[listTop], name);
-    listTop++;
-    return listTop - 1;
+    return listTop++;
 }
 
 void createGraph(AdjPtr *graph, int r_num, char (*refList)[4]) {
     int src, dest, cap;
     char srcStr[4], destStr[4];
-    AdjPtr temp_ptr;
-
     for (int i = 0; i < r_num; i++) {
         scanf("%3s %3s %d", srcStr, destStr, &cap);
         src = getIndex(srcStr, refList);
         dest = getIndex(destStr, refList);
-
-        temp_ptr = (AdjPtr)calloc(1, sizeof(AdjList));
-        temp_ptr->edgeCap = cap;
-        temp_ptr->nextV = dest;
-        temp_ptr->flow = 0; 
-        temp_ptr->Next = graph[src];
-        graph[src] = temp_ptr;
+        AdjPtr edge = calloc(1, sizeof(AdjList));
+        edge->nextV = dest;
+        edge->edgeCap = cap;
+        edge->Next = graph[src];
+        graph[src] = edge;
     }
 }
 
-AdjPtr *createResidualGraph(AdjPtr *graph, int nodeCount) {
-    AdjPtr *residualGraph = (AdjPtr *)calloc(nodeCount, sizeof(AdjPtr));
-    for (int i = 0; i < nodeCount; i++) {
-        AdjPtr temp = graph[i];
-        while (temp) {
-            AdjPtr forwardEdge = (AdjPtr)calloc(1, sizeof(AdjList));
-            forwardEdge->nextV = temp->nextV;
-            forwardEdge->edgeCap = temp->edgeCap;
-            forwardEdge->flow = 0;
-            forwardEdge->Next = residualGraph[i];
-            residualGraph[i] = forwardEdge;
-
-            AdjPtr reverseEdge = (AdjPtr)calloc(1, sizeof(AdjList));
-            reverseEdge->nextV = i;
-            reverseEdge->edgeCap = 0;
-            reverseEdge->flow = 0;
-            reverseEdge->Next = residualGraph[temp->nextV];
-            residualGraph[temp->nextV] = reverseEdge;
-
-            temp = temp->Next;
+AdjPtr* createResidualGraph(AdjPtr *graph, int nodeCount) {
+    AdjPtr *residual = calloc(nodeCount, sizeof(AdjPtr));
+    for (int u = 0; u < nodeCount; u++) {
+        for (AdjPtr e = graph[u]; e; e = e->Next) {
+            AdjPtr fe = calloc(1, sizeof(AdjList));
+            fe->nextV = e->nextV;
+            fe->edgeCap = e->edgeCap;
+            fe->Next = residual[u];
+            residual[u] = fe;
+            AdjPtr re = calloc(1, sizeof(AdjList));
+            re->nextV = u;
+            re->edgeCap = 0;
+            re->Next = residual[e->nextV];
+            residual[e->nextV] = re;
         }
     }
-
-    return residualGraph;
+    return residual;
 }
 
 bool dfs(AdjPtr *residualGraph, int *visited, int src, int dest, int *parent) {
     visited[src] = 1;
     if (src == dest) return true;
-
-    AdjPtr temp = residualGraph[src];
-    while (temp) {
-        int next = temp->nextV;
-        if (!visited[next] && (temp->edgeCap - temp->flow > 0)) {
-            parent[next] = src;
-            if (dfs(residualGraph, visited, next, dest, parent)) {
+    for (AdjPtr e = residualGraph[src]; e; e = e->Next) {
+        int v = e->nextV;
+        if (!visited[v] && e->edgeCap > 0) {
+            parent[v] = src;
+            if (dfs(residualGraph, visited, v, dest, parent))
                 return true;
-            }
         }
-        temp = temp->Next;
     }
     return false;
 }
@@ -129,68 +112,45 @@ bool dfs(AdjPtr *residualGraph, int *visited, int src, int dest, int *parent) {
 void updateResidualGraph(AdjPtr *residualGraph, int *parent, int src, int dest, int pathFlow) {
     for (int v = dest; v != src; v = parent[v]) {
         int u = parent[v];
-
-        AdjPtr temp = residualGraph[u];
-        while (temp && temp->nextV != v) {
-            temp = temp->Next;
-        }
-        if (temp) {
-            temp->flow += pathFlow;
-            temp->edgeCap -= pathFlow;
-        }
-
-        temp = residualGraph[v];
-        while (temp && temp->nextV != u) {
-            temp = temp->Next;
-        }
-        if (temp) {
-            temp->edgeCap += pathFlow;
-        }
+        AdjPtr e;
+        for (e = residualGraph[u]; e && e->nextV != v; e = e->Next);
+        if (e) e->edgeCap -= pathFlow;
+        for (e = residualGraph[v]; e && e->nextV != u; e = e->Next);
+        if (e) e->edgeCap += pathFlow;
     }
 }
 
 int fordFulkerson(AdjPtr *graph, int nodeCount, int src, int dest) {
-    AdjPtr *residualGraph = createResidualGraph(graph, nodeCount);
+    AdjPtr *residual = createResidualGraph(graph, nodeCount);
     int maxFlow = 0;
-    int *parent = (int *)calloc(nodeCount, sizeof(int));
-    int *visited = (int *)calloc(nodeCount, sizeof(int));
-
-    while (1) {
+    int *parent = calloc(nodeCount, sizeof(int));
+    int *visited = calloc(nodeCount, sizeof(int));
+    while (true) {
         memset(visited, 0, nodeCount * sizeof(int));
         memset(parent, -1, nodeCount * sizeof(int));
-
-        if (!dfs(residualGraph, visited, src, dest, parent)) {
+        if (!dfs(residual, visited, src, dest, parent))
             break;
-        }
-
         int pathFlow = INT_MAX;
         for (int v = dest; v != src; v = parent[v]) {
             int u = parent[v];
-            AdjPtr temp = residualGraph[u];
-            while (temp && temp->nextV != v) {
-                temp = temp->Next;
-            }
-            if (temp) {
-                pathFlow = (temp->edgeCap - temp->flow < pathFlow) ? (temp->edgeCap - temp->flow) : pathFlow;
-            }
+            AdjPtr e;
+            for (e = residual[u]; e && e->nextV != v; e = e->Next);
+            if (e && e->edgeCap < pathFlow)
+                pathFlow = e->edgeCap;
         }
-
-        updateResidualGraph(residualGraph, parent, src, dest, pathFlow);
-
+        updateResidualGraph(residual, parent, src, dest, pathFlow);
         maxFlow += pathFlow;
     }
-
     for (int i = 0; i < nodeCount; i++) {
-        AdjPtr temp = residualGraph[i];
-        while (temp) {
-            AdjPtr toFree = temp;
-            temp = temp->Next;
-            free(toFree);
+        AdjPtr e = residual[i];
+        while (e) {
+            AdjPtr t = e;
+            e = e->Next;
+            free(t);
         }
     }
-    free(residualGraph);
+    free(residual);
     free(parent);
     free(visited);
-
     return maxFlow;
 }
