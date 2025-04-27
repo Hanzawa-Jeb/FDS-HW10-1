99b64f9b1f718a6f6f202e3ce914a1aa50d9b18d
Jeb_0816 <hz_legoman@hotmail.com>
2025-04-27 10:55:31 +0800

    Version 1.0

--- UniqueMST.c
+++ UniqueMST.c
@@ -2,6 +2,35 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+typedef struct{
+    int u;
+    int v;
+    int weight;
+} edge;
+//we use edge to store the graph, which is suitable for the kruskal algorithm.
+typedef edge * Graph;
+
+void createGraph(Graph graph, int edgeCnt);
+int cmp(const void *a, const void *b);
+//the compare function used in the qsort
+
 int main() {
-    
+    int vertexCnt, edgeCnt;
+    scanf("%d %d", &vertexCnt, &edgeCnt);
+    Graph graph = (Graph)calloc(edgeCnt, sizeof(edge));
+    createGraph(graph, edgeCnt);
+    qsort(graph, edgeCnt, sizeof(edge), cmp);
+}
+
+void createGraph(Graph graph, int edgeCnt) {
+    for (int i = 0; i < edgeCnt; i ++) {
+        scanf("%d %d %d", (graph+i)->u, (graph+i)->v, (graph+i)->weight);
+    }
+}
+
+int cmp(const void *a, const void *b)
+{
+    edge * temp_a = (edge *)a;
+    edge * temp_b = (edge *)b;
+    return temp_a->weight - temp_b->weight;
 }
